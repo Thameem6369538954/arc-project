540ba06e484b757c81f7daeb63757bfd9ba81025
Thameem Ansari <thameemrpp@gmail.com>
2025-02-21 00:08:02 +0530

    first committa

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,25 @@ import SmoothScroll from "./Components/SmoothScroll";
 
 function App() {
   const [showMainContent, setShowMainContent] = useState(false);
+  const isMobile = window.innerWidth < 768; // Detect mobile devices
 
   useEffect(() => {
     setTimeout(() => setShowMainContent(true), 2000);
   }, []);
 
   return (
-    <Suspense>
-      {!showMainContent ? <SlidingOpenEffect /> : (
-        <SmoothScroll> {/* Move data-scroll-container inside here */}
+    <Suspense fallback={<div>Loading...</div>}>
+      {!showMainContent ? (
+        <SlidingOpenEffect />
+      ) : isMobile ? (
+        <>
+          <ScrollToTop />
+          <Navbar />
+          <AllRouter />
+          <Footer />
+        </>
+      ) : (
+        <SmoothScroll> {/* Apply smooth scrolling only for non-mobile devices */}
           <ScrollToTop />
           <Navbar />
           <AllRouter />
